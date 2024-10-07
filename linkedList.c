a267e994420d963a163a36994d9d827d96394e5d
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-10-07 15:53:23 +0530

    operation on linked list

--- linkedList.c
+++ linkedList.c
@@ -1,48 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node
-{
+struct Node {
     int data;
     struct Node* next;
 };
 
-void linkedListTraversal(struct Node* ptr){
-    if (ptr == NULL)
-    {
-        printf("Linked List is empty");
+void linkedListTraversal(struct Node* ptr) {
+    if (ptr == NULL) {
+        printf("Linked List is empty\n");
         return;
     }
 
-    while (ptr != NULL)
-    {
-        printf("Elements: %d\n", ptr->data);
+    while (ptr != NULL) {
+        printf("Element: %d\n", ptr->data);
         ptr = ptr->next;
     }
-    
-    
 }
 
-struct Node* insertAtBeggining(struct Node* ptr, int data){
+struct Node* insertAtBeginning(struct Node* ptr, int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
 
-    if (ptr == NULL)
-    {
-        newNode->next = NULL;
-        return newNode;
-    }
-
     newNode->next = ptr;
     return newNode;
-    
 }
 
-// struct Node* insertAtIdx(struct Node* ptr, int data)
+struct Node* insertAtIdx(struct Node* ptr, int data, int idx) {
+    if (idx == 0) {
+        return insertAtBeginning(ptr, data);
+    }
+    if (ptr == NULL) {
+        printf("Can't insert at any index of the empty linked list\n");
+        return NULL;
+    }
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+
+    struct Node* temp = ptr;
+
+    int count = 0;
+    while (count < idx - 1 && temp != NULL) {
+        temp = temp->next;
+        count++;
+    }
+    if (temp == NULL) {
+        printf("Index is out of bounds\n");
+        free(newNode);
+        return ptr;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    return ptr;
+}
+
+struct Node* insertAtEnd(struct Node* ptr, int data) {
+    if (ptr == NULL) {
+        return insertAtBeginning(ptr, data);
+    }
+
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    struct Node* temp = ptr;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return ptr;
+}
+
+struct Node* insertAfterNode(struct Node* ptr, )
 
 int main() {
     struct Node* head = NULL;
-    head = insertAtBeggining(head, 10);
-    head = insertAtBeggining(head, 20);
+    head = insertAtBeginning(head, 10);
+    head = insertAtBeginning(head, 20);
+    head = insertAtIdx(head, 12, 0);
+    head = insertAtIdx(head, 22, 1);
+    head = insertAtEnd(head, 45);
     linkedListTraversal(head);
+
+    return 0;
 }
